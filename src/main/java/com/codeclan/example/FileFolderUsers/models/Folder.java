d78ccdba0a9263689d8cda4c352d2d3a96a8ba26
Stephen Blythe <stephenblythe@gmail.com>
2019-10-08 16:17:41 +0100

    Sets up basic folder class

--- src/main/java/com/codeclan/example/FileFolderUsers/models/Folder.java
+++ src/main/java/com/codeclan/example/FileFolderUsers/models/Folder.java
@@ -1,4 +1,44 @@
 package com.codeclan.example.FileFolderUsers.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "folders")
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column
+    private String name;
+
+
+
+
+    public Folder() {
+    }
+
+    public Folder(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
